a0231fc42285c811ea07fc70f7793b66de22239c
Sfournio <44951701+bycop@users.noreply.github.com>
2021-11-19 12:51:42 +0100

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,7 +6,7 @@ module.exports = async (client, interaction) => {
 
 	let cmd = interaction.commandName.toLowerCase();
 
-	let commandfile = client.commandes.get(cmd) || client.commandes.get(client.aliases.get(cmd));
+	let commandfile = client.commands.get(cmd);
 	if (commandfile) {
 		commandfile.run(client, interaction, args, cmd);
 	}
